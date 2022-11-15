8ffb920159b90aaa9d1df3899508fd044e919228
neverdieone <altiore@mail.ru>
2022-11-15 14:34:54 +0000

    Add conversation persistence

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import logging
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from environs import Env
+from enum import StrEnum, auto
+from textwrap import dedent
 
+from environs import Env
+from telegram import Update
+from telegram.ext import (Application, CommandHandler, ContextTypes,
+                          ConversationHandler, PicklePersistence)
 
 logger = logging.getLogger(__name__)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+class States(StrEnum):
+    MENU = auto()
 
+
+async def start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+) -> States:
+    await update.message.reply_text(
+        dedent('''\
+        Привет! Этот бот помогает в ежедневных тренировках.
+        Он умеет напоминать о том что нужно сделать зарядку,
+        а так же показывает этапы её прохождения.
+        ''')
+    )
+    return States.MENU
 
 def main() -> None:
     logging.basicConfig(
@@ -25,8 +36,28 @@ def main() -> None:
     env = Env()
     env.read_env()
 
-    application = Application.builder().token(env.str('TG_BOT_TOKEN')).build()
-    application.add_handler(CommandHandler("start", start))
+    persistence = PicklePersistence('data.pickle')
+
+    application = Application\
+        .builder()\
+        .token(env.str('TG_BOT_TOKEN'))\
+        .persistence(persistence)\
+        .concurrent_updates(True)\
+        .build()
+
+    conv_handler = ConversationHandler(
+        entry_points=[
+            CommandHandler('start', start)
+        ],
+        states={
+            States.MENU: []
+        },
+        fallbacks=[],
+        persistent=True,
+        name='main_conversation'
+    )
+
+    application.add_handler(conv_handler)
     application.run_polling()
 
 
